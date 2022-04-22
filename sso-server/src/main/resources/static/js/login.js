d1da4a5a19d17be12b344e8111a076eb9564b23f
cy <51856506+ChiYouIc@users.noreply.github.com>
2022-04-22 21:34:46 +0800

    更新登录页

---
+++ sso-server/src/main/resources/static/js/login.js
@@ -0,0 +1,31 @@
+let originUrl = '';
+
+window.onload = function () {
+    const url = window.location.href;
+    const pattern = new RegExp('originUrl=[a-zA-z]+://[^\\s]*', 'i');
+    const origin = pattern.exec(url)[0];
+    originUrl = origin.substr(origin.indexOf('=') + 1, origin.length)
+}
+
+function login() {
+
+    let username = $('#username').val();
+    let password = $('#password').val();
+
+    console.log(username, password)
+
+    $.ajax({
+        url: '/sso/login',
+        type: 'POST',
+        contentType: 'application/json;charset=utf-8',
+        param: {"originUrl": originUrl},
+        data: JSON.stringify({'username': username, 'password': password}),
+        dataType: 'text',
+        success: function (result) {
+
+        },
+        error: function (msg) {
+
+        }
+    })
+}
